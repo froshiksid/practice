08c9a615d294f89ce0bf223e4ada60f0d16b85f7
froshiksid <aviktorovna619@gmail.com>
2021-10-01 13:36:33 +0300

    практика 1

---
+++ практика 1/lab1.py
@@ -0,0 +1,16 @@
+# This is the comment for the comments.py file
+print("Game over!")  # this comment is for the second line
+print("# this is not a comment")
+# add new comment here
+input("\n\n Enter any button to exit.")
+
+name = "Настя"
+secondName = "Толмачева"
+group = "КЭ-103"
+print(name, secondName, group)
+
+af = "Афоризм — это хорошо отредактированный роман."
+au = "Владимир Голобородько"
+probel = " " * 12
+print(af)
+print(probel + au)
